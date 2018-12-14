70e65a0ab225ccc75fd921e687ff0c48c834414e
Artem Melnyk <temmar1991@gmail.com>
2018-12-14 22:42:56 +0200

    Regullar expressions fix 3

--- regullar_expressions.py
+++ regullar_expressions.py
@@ -1,16 +1,22 @@
+
 import re
 import os
+import pdb
+
+
 
 
 def run_re():
     pattern = 'pDq'
 
     temp_dir = '/tmp/'
+    pdb.set_trace()
     filename = os.path.join(temp_dir, 'large_re_file.txt')
     infile = open(filename, 'r')
     match_count = 0
     lines = 0
 
+    pdb.set_trace()
     for line in infile:
         match = re.search(pattern, line)
         if match:
@@ -19,8 +25,10 @@ def run_re():
     return lines, match_count
 
 
-
+pdb.set_trace()
 if __name__ == "main":
+    pdb.set_trace()
     lines, match_count = run_re()
+    pdb.set_trace()
     print('LINES::, {}'.format(lines))
-    print('MATCHES::,{}'.format(match_count))
+    print('MATCHES::, {}'.format(match_count))
