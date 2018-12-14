9ebd3b7c1e45ec9d9e543d2c166d0baa64d19379
Artem Melnyk <temmar1991@gmail.com>
2018-12-14 02:01:39 +0200

    Regullar expressions fix 2

--- regullar_expressions.py
+++ regullar_expressions.py
@@ -1,13 +1,31 @@
 import re
+<<<<<<< HEAD
 import os
 
+=======
+<<<<<<< HEAD
+import os
+
+=======
+>>>>>>> 222f6009832b976bb9d9a93603fe623dd4a787d9
+>>>>>>> f1e02f7fff9a598cb5d74c2ef3dba691ef1985af
 
 def run_re():
     pattern = 'pDq'
 
+<<<<<<< HEAD
     temp_dir = '/tmp/'
     filename = os.path.join(temp_dir, 'large_re_file.txt')
     infile=open(filename,'r')
+=======
+<<<<<<< HEAD
+    temp_dir = '/tmp/'
+    filename = os.path.join(temp_dir, 'large_re_file.txt')
+    infile=open(filename,'r')
+=======
+    infile=open('large_re_file.txt','r')
+>>>>>>> 222f6009832b976bb9d9a93603fe623dd4a787d9
+>>>>>>> f1e02f7fff9a598cb5d74c2ef3dba691ef1985af
     match_count = 0
     lines = 0
     for line in infile:
@@ -17,7 +35,14 @@ def run_re():
             line += 1
     return (lines, match_count)
 
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+
+=======
+>>>>>>> 222f6009832b976bb9d9a93603fe623dd4a787d9
+>>>>>>> f1e02f7fff9a598cb5d74c2ef3dba691ef1985af
 if __name__ == "main":
     lines, match_count = run_re()
     print('LINES::, {}'.format(lines))
